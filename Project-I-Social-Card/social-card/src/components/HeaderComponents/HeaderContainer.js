fb383a3cf19ecfe27b201d85bb897f00adb6d1ac
Allen Babiarz <ababiarz@Allen-Babiarzs-iMac.local>
2019-06-05 16:22:04 -0500

    Header content added

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+import Img from './ImageThumbnail';
+import Title from './HeaderTitle';
+
+function Header() {
+    return(
+        <div>
+            <Img />
+            <Title />
+        </div>
+    )
+}
+
+export default Header;
--- Project-I-Social-Card/social-card/src/index.js
+++ Project-I-Social-Card/social-card/src/index.js
@@ -2,5 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Header from './components/HeaderComponents/HeaderContainer';
 
+function I() {
+    return (
+        <div>
+            <Header />
+        </div>
+    );
+}
+
+export default I;
 ReactDOM.render(<App />, document.getElementById('root'));
